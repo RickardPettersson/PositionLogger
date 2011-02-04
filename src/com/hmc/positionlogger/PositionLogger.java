a12830a6523fdc24bc795659ca9aaf5cacb794c9
Rickard Pettersson <rickardnp@gmail.com>
2011-02-04 10:35:12 +0100

    Fixed the buttons and the service function to work together and added a README.md file

--- src/com/hmc/positionlogger/PositionLogger.java
+++ src/com/hmc/positionlogger/PositionLogger.java
@@ -3,25 +3,30 @@ package com.hmc.positionlogger;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class PositionLogger extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    public void startListening(View v) {
-        Intent i=new Intent(this, PositionService.class);
-        
-        i.putExtra(PositionService.listeningOnGPS, "true");
-        i.putExtra(PositionService.listeningOnNetwork, "true");
-        
-        startService(i);
-      }
-      
-      public void stopListening(View v) {
-        stopService(new Intent(this, PositionService.class));
-      }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void startListening(View v) {
+		Log.w(getClass().getName(), "Start listening click");
+		
+		Intent i = new Intent(this, PositionService.class);
+
+		i.putExtra(PositionService.listeningOnGPS, "true");
+		i.putExtra(PositionService.listeningOnNetwork, "true");
+		
+		startService(i);
+	}
+
+	public void stopListening(View v) {
+		Log.w(getClass().getName(), "Stop listening click");
+		stopService(new Intent(this, PositionService.class));
+	}
 }

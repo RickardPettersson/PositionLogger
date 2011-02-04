a70ebdb79b390aa1a71e953d28c9e12ba21f3a34
Marco Alanen <marco.alanen@sbg.se>
2011-02-04 17:49:17 +0800

    - Started integrating ActionBar.

--- src/com/hmc/positionlogger/PositionLogger.java
+++ src/com/hmc/positionlogger/PositionLogger.java
@@ -1,12 +1,40 @@
 package com.hmc.positionlogger;
 
+import com.markupartist.android.widget.ActionBar;
+import com.markupartist.android.widget.ActionBar.IntentAction;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class PositionLogger extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
+        actionBar.setHomeAction(new IntentAction(this, PositionLogger.createIntent(this), R.drawable.home));
+        actionBar.addAction(new IntentAction(this, PositionLogger.createIntent(this), R.drawable.layers));
+    }
+
+    public void startListening(View v) {
+        Intent i=new Intent(this, PositionService.class);
+        
+        i.putExtra(PositionService.listeningOnGPS, "true");
+        i.putExtra(PositionService.listeningOnNetwork, "true");
+        
+        startService(i);
+      }
+      
+      public void stopListening(View v) {
+        stopService(new Intent(this, PositionService.class));
+      }
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,4 +57,10 @@ public class PositionLogger extends Activity {
 		Log.w(getClass().getName(), "Stop listening click");
 		stopService(new Intent(this, PositionService.class));
 	}
+      
+    public static Intent createIntent(Context context) {
+    	Intent intent = new Intent(context, PositionLogger.class);
+    	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    	return intent;
+    }
 }
